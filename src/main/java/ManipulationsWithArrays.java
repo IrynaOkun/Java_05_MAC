822b892096d8310d255f63d22c48aa5ee1b2086d
IrynaOkun <iryna.ladybug@gmail.com>
2022-10-26 07:26:35 +0300

    manipArrays_task_5

--- src/main/java/ManipulationsWithArrays.java
+++ src/main/java/ManipulationsWithArrays.java
@@ -1,4 +1,29 @@
+import java.util.Arrays;
+
 public class ManipulationsWithArrays {
+
+    /** Task_5
+     * Написать метод multiplуArrayByNumber(), который принимает на вход массив целых чисел и число int number.
+     * Метод возвращает массив тех же чисел, умноженных на number
+     * Например, multiplуArrayByNumber()({1, 2, 3, 4, 5}, 3) -> {3, 6, 9, 12, 15}
+     */
+
+    public int[] multiplуArrayByNumber(int[] array, int number) {
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (i+1)  * number;
+        }
+
+        return array;
+    }
+
+    public static void main(String[] args) {
+
+//        int[] array = {1, 2, 3, 4, 5};
+//        int number = 10;
+//
+//        multiplуArrayByNumber(array, number);
+    }
 
 
 }
--- src/test/java/ManipulationsWithArraysTest.java
+++ src/test/java/ManipulationsWithArraysTest.java
@@ -1,4 +1,34 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class ManipulationsWithArraysTest {
+
+    @Test
+    //Task_5
+    public void testmultiplуArrayByNumber() {
+
+        int[] array = {1, 2, 3, 4, 5};
+        int number = 10;
+
+        int[] actualResult = {10, 20, 30, 40, 50};
+
+        int[] expectedResult = new ManipulationsWithArrays().multiplуArrayByNumber(array, number);
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testmultiplуArrayByNumberThree() {
+
+        int[] array = {1, 2, 3, 4, 5};
+        int number = 3;
+
+        int[] actualResult = {3, 6, 9, 12, 15};
+
+        int[] expectedResult = new ManipulationsWithArrays().multiplуArrayByNumber(array, number);
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
 
 
 }
